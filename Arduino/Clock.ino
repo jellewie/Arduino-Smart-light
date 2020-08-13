23c9c2982fb323f310e709f4a8f72e4c3a9433d1
JelleWie <1545223+jellewie@users.noreply.github.com>
2020-08-13 21:35:18 +0200

    Fixed; blink if NOT connected

--- Arduino/Clock.ino
+++ Arduino/Clock.ino
@@ -34,7 +34,7 @@ void UpdateAndShowClock(bool ShowClock) {
     } else
       TimeFlag = false;
   }
-  if (WiFiManager_connected)                    //If we are no longer connected to WIFI
+  if (!WiFiManager_connected)                    //If we are no longer connected to WIFI
     if (TickEveryMS(2000)) digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); //Blink every 2 second to show we have lost WIFI and can not sync
   //==============================
   //Show the time on the LEDs if needed
