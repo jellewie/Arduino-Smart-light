cc0078904b5d77a078ac9a90d43a423696abeedc
JelleWie <1545223+jellewie@users.noreply.github.com>
2020-08-20 18:48:21 +0200

    Constrain change time command values to always be valid

--- Arduino/handler.ino
+++ Arduino/handler.ino
@@ -299,13 +299,13 @@ void handle_UpdateTime() {
       byte ArgValue = server.arg(i).toInt();
       if (ArguName == PreFixTimeHour) {
         TimeUpdated = true;
-        TimeCurrent.HH = ArgValue;
+        TimeCurrent.HH = constrain(ArgValue, 0, 23);;
       } else if (ArguName == PreFixTimeMin) {
         TimeUpdated = true;
-        TimeCurrent.MM = ArgValue;
+        TimeCurrent.MM = constrain(ArgValue, 0, 59);
       } else if (ArguName == PreFixTimeSec) {
         TimeUpdated = true;
-        TimeCurrent.SS = ArgValue;
+        TimeCurrent.SS = constrain(ArgValue, 0, 59);
       } else
         ERRORMSG += "Unknown arg '" + ArguName + "' with value '" + ArgValue + "'\n";
     }
